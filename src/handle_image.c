d814f31cd83595478391aedac9b86e8ccb1712e0
MiniKlar <omontloris@gmail.com>
2025-11-09 03:19:47 +0100

    - Added comments to texture loading paths for futur merge.

--- src/handle_image.c
+++ src/handle_image.c
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 06:20:36 by lomont            #+#    #+#             */
-/*   Updated: 2025/11/09 01:13:10 by lomont           ###   ########.fr       */
+/*   Updated: 2025/11/09 02:33:00 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ void	create_image(t_app *app)
 	app->img = mlx_new_image(app->mlx, 1920, 1080);
 	if (!app->img || (mlx_image_to_window(app->mlx, app->img, 0, 0) < 0))
 		exit(1);
-	app->textures[0] = mlx_load_png("./models/east_model.png");
+	app->textures[0] = mlx_load_png("./models/east_model.png"); //"east_path"
 	if (!app->textures[0])
 		exit(2);
-	app->textures[1] = mlx_load_png("./models/north_model.png");
+	app->textures[1] = mlx_load_png("./models/north_model.png"); //"north_path"
 	if (!app->textures[1])
 		exit(3);
-	app->textures[2] = mlx_load_png("./models/south_model.png");
+	app->textures[2] = mlx_load_png("./models/south_model.png"); //"south_path"
 	if (!app->textures[2])
 		exit(4);
-	app->textures[3] = mlx_load_png("./models/west_model.png");
+	app->textures[3] = mlx_load_png("./models/west_model.png"); //"west_path"
 	if (!app->textures[3])
 		exit(5);
 }
