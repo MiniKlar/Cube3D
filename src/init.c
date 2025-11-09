99925424ce1c055a9c74142fcb1d467c98cfe0d4
MiniKlar <omontloris@gmail.com>
2025-11-09 01:20:30 +0100

    - Refactored init_app and init_player functions for improved player initialization and clarity

--- src/init.c
+++ src/init.c
@@ -6,38 +6,39 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 00:41:15 by lomont            #+#    #+#             */
-/*   Updated: 2025/11/07 01:15:44 by lomont           ###   ########.fr       */
+/*   Updated: 2025/11/09 01:03:30 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/exec.h"
 
-bool init_mlx(mlx_t **mlx)
+static void	init_player(t_player *player)
 {
-	*mlx = mlx_init(1920, 1080, "Cube3D", true);
-	if (!mlx)
+	player->pos_x = 22;
+	player->pos_y = 12;
+	player->dir_x = -1;
+	player->dir_y = 0;
+	player->plane_x = 0;
+	player->plane_y = 0.66;
+	player->move_speed = 0.05;
+	player->rot_speed = 0.03;
+}
+
+static bool	init_mlx(t_app *app)
+{
+	app->mlx = mlx_init(1920, 1080, "Cube3D", true);
+	if (!app->mlx)
 		return (false);
 	return (true);
 }
 
-bool init_app(t_app *app)
+bool	init_app(t_app *app)
 {
-	if (app)
-	{
-		if (!init_mlx(&app->mlx))
-			return (false);
-		app->posX = 22;
-		app->posY = 12;
-		app->dirX = -1;
-		app->dirY = 0;
-		app->planeX = 0;
-		app->planeY = 0.66;
-		app->time = 0;
-		app->move_speed = 0;
-		app->rot_speed = 0;
-		app->img = NULL;
-		app->fps_img = NULL;
-		app->textures[0] = NULL;
-	}
+	if (!app)
+		return (false);
+	ft_bzero(app, sizeof(t_app));
+	if (!init_mlx(app))
+		return (false);
+	init_player(&app->player);
 	return (true);
 }
